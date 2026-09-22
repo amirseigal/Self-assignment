80d695157c5b467e5d4e699dd46a705044bab195
Amirreza Seighali <138161564+amirseigal@users.noreply.github.com>
2026-09-22 17:49:46 +0200

    Add files via upload

---
+++ Tasks-Week1/task2.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+
+    zahl = 10                  
+    kommazahl = 10.5           
+    text = "Hello, World!"     
+    wahrheitswert = True       
+
+    print("Integer zahl:", zahl, "- type:", type(zahl))
+    print("Float kommazahl:", kommazahl, "- type:", type(kommazahl))
+    print("String text:", text, "- type:", type(text))
+    print("Boolean wahrheitswert:", wahrheitswert, "- type:", type(wahrheitswert))
